f39a5dd68874df4e4b128e46bfe7d5744c72620d
Trugamr <trugamr@live.in>
2023-02-12 02:23:42 +0530

    add search channels tab

--- scripts/twitch.ts
+++ scripts/twitch.ts
@@ -100,6 +100,7 @@ api.interceptors.request.use(config => {
 
 api.interceptors.response.use(undefined, async error => {
   if (axios.isAxiosError(error) && error.response.status === 401) {
+    console.log('Refreshing credentials')
     const { data: credentials } = await post<TwitchCredentials>(
       'https://id.twitch.tv/oauth2/token',
       undefined,
@@ -126,49 +127,101 @@ api.interceptors.response.use(undefined, async error => {
   return Promise.reject(error)
 })
 
-const {
-  data: {
-    data: [me],
-  },
-} = await api.get<{ data: Array<{ id: string }> }>('/users')
+onTab('Followed', async () => {
+  console.log('Fetching profile')
+  const {
+    data: {
+      data: [me],
+    },
+  } = await api.get<{ data: Array<{ id: string }> }>('/users')
 
-const {
-  data: { data: streams },
-} = await api.get<{
-  data: Array<{
-    id: string
-    user_login: string
-    user_name: string
-    title: string
-    viewer_count: number
-    started_at: string
-    thumbnail_url: string
-    game_name: string
-  }>
-}>('/streams/followed', {
-  params: {
-    user_id: me.id,
-  },
+  console.log('Fetching followed streams')
+  const {
+    data: { data: streams },
+  } = await api.get<{
+    data: Array<{
+      id: string
+      user_login: string
+      user_name: string
+      title: string
+      viewer_count: number
+      started_at: string
+      thumbnail_url: string
+      game_name: string
+    }>
+  }>('/streams/followed', {
+    params: {
+      user_id: me.id,
+    },
+  })
+
+  const stream = await arg<(typeof streams)[number]>({
+    placeholder: 'Search followed stream',
+    choices: streams.map(s => {
+      const img = s.thumbnail_url
+        .replace('{width}', '160')
+        .replace('{height}', '90')
+
+      return {
+        name: `${s.user_name} • ${s.title}`,
+        description: `${s.viewer_count.toLocaleString()} watching ${
+          s.user_name
+        } play ${s.game_name}, started ${formatDateToNow(
+          new Date(s.started_at),
+          { addSuffix: true },
+        )}`,
+        img,
+        value: s,
+      }
+    }),
+  })
+
+  await browse(`https://twitch.tv/${stream.user_login}`)
 })
 
-const stream = await arg<(typeof streams)[number]>({
-  placeholder: 'Search followed stream',
-  choices: streams.map(s => {
-    const img = s.thumbnail_url
-      .replace('{width}', '160')
-      .replace('{height}', '90')
+onTab('Search', async () => {
+  const broadcaster = await arg({
+    placeholder: 'Search stream',
+    choices: async input => {
+      if (input.trim() === '') {
+        return []
+      }
 
-    return {
-      name: `${s.user_name} • ${s.title}`,
-      description: `${s.viewer_count.toLocaleString()} watching ${
-        s.user_name
-      } play ${s.game_name}, started ${formatDateToNow(new Date(s.started_at), {
-        addSuffix: true,
-      })}`,
-      img,
-      value: s,
-    }
-  }),
+      const {
+        data: { data: channels },
+      } = await api.get<{
+        data: Array<{
+          broadcaster_login: string
+          display_name: string
+          game_id: string
+          game_name: string
+          id: string
+          tags: string[] | null
+          thumbnail_url: string
+          title: string
+          started_at: string
+        }>
+      }>('/search/channels', {
+        params: {
+          query: input,
+          live_only: true,
+        },
+      })
+
+      return channels.map(c => {
+        return {
+          name: c.title,
+          description: `${c.display_name} is playing ${
+            c.game_name
+          }, started ${formatDateToNow(new Date(c.started_at), {
+            addSuffix: true,
+          })}`,
+          img: c.thumbnail_url,
+          value: c.broadcaster_login,
+        }
+      })
+    },
+  })
+
+  await browse(`https://twitch.tv/${broadcaster}`)
 })
-
-await browse(`https://twitch.tv/${stream.user_login}`)
--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,16 +1,12 @@
 import { PromptConfig, Shortcut } from '@johnlindquist/kit'
 
-function uniq<T>(value: T[]) {
-  return [...new Set(value)]
-}
-
 /**
  * Remove duplicate and kit's path entries
  * Kit's path entries override installed npm and node
  */
 export function pathfix() {
   if (process.env.PATH) {
-    process.env.PATH = uniq(process.env.PATH.split(path.delimiter))
+    process.env.PATH = _.uniq(process.env.PATH.split(path.delimiter))
       .filter(
         entry => !['.knode', '.kit', '.kenv'].some(k => entry.includes(k)),
       )
