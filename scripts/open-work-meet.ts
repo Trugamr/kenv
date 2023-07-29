edb4ff08162214af9c3cb3f0bd51ae171aff6b64
Shivam Dua <trugamr@live.in>
2023-07-29 19:47:06 +0530

    feature: add script to open new google meet in browser

---
+++ scripts/open-work-meet.ts
@@ -0,0 +1,9 @@
+// Name: Open Work Meet
+// Description: Open new google meet in browser
+
+import '@johnlindquist/kit'
+
+const url = 'https://meet.google.com/new?authuser=1'
+
+console.log('Opening in browser...')
+open(url)
